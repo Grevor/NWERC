e26f09ae4e1917c84ed77d8f020cca7f25eeaf5a
Grevor <Grevor.priest@gmail.com>
2013-11-06 16:47:32 +0100

    Now with some more methods...

--- GraphsLib/src/staticvoid/graphs/EdgeGraph.java
+++ GraphsLib/src/staticvoid/graphs/EdgeGraph.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.PriorityQueue;
+import java.util.Random;
 
 public class EdgeGraph implements Graph {
 	private static final int estimatedEdges = 100;
@@ -21,10 +22,23 @@ public class EdgeGraph implements Graph {
 		return this.allNodes.size();
 	}
 	
-	public void createEdge(int startID, int targetID, double weight, boolean createBothWays) {
+	public boolean hasEdge(int s, int e) {
+		Node n = allNodes.get(s);
+		for(int i = 0; i < n.getNumberOfEdges(); i++) {
+			if(n.getEdge(i).getTargetNode().nodeID == e)
+				return true;
+		}
+		return false;
+	}
+	
+	public boolean createEdge(int startID, int targetID, double weight, boolean createBothWays) {
+		if(startID == targetID || this.hasEdge(startID, targetID))
+			return false;
+		
 		this.allNodes.get(startID).addEdge(new Edge(this.allNodes.get(targetID), weight));
 		if(createBothWays)
 			this.allNodes.get(targetID).addEdge(new Edge(this.allNodes.get(startID), weight));
+		return true;
 	}
 	
 	public ShortestWay getShortestPathDjikstra(int startID, int endID) {
@@ -72,6 +86,38 @@ public class EdgeGraph implements Graph {
 		}
 		
 		return null;
+	}
+	
+	
+	
+	public double[][] getShortestPaths() {
+		double[][] allDist = new double[this.getNodeCount()][this.getNodeCount()];
+		for (int l = 0; l < allDist.length; l++) {
+			for (int l2 = 0; l2 < allDist[0].length; l2++) {
+				allDist[l][l2] = Double.POSITIVE_INFINITY;
+			}
+		}
+		for (int l = 0; l < allDist.length; l++) {
+			allDist[l][l] = 0;
+		}
+		
+		for (int l = 0; l < allDist.length; l++) {
+			for (int j = 0; j < allNodes.get(l).getNumberOfEdges(); j++) {
+				allDist[l][allNodes.get(l).getEdge(j).targetNode.nodeID] =
+						allNodes.get(l).getEdge(j).weight;
+			}
+		}
+
+		for (int k = 0; k < allDist.length; k++) {
+			for (int i = 0; i < allDist.length; i++) {
+				for (int j = 0; j < allDist.length; j++) {
+					if (allDist[i][k] + allDist[k][j] < allDist[i][j]) { 
+		            allDist[i][j] =  allDist[i][k] + allDist[k][j];
+					}
+				}
+			}
+		}
+		return allDist;
 	}
 	
 	
@@ -143,7 +189,7 @@ public class EdgeGraph implements Graph {
 			this.edges.add(e);
 		}
 	}
-	
+	/*
 	public static void main(String[] args) {
 		EdgeGraph g = new EdgeGraph(10);
 		g.addNode();
@@ -162,12 +208,29 @@ public class EdgeGraph implements Graph {
 		g.createEdge(0, 4, 10163, true);
 		g.createEdge(4, 5, 104, false);
 		
+		g = getGraph(1000000, 3000000, 40);
+		long start = System.nanoTime();
 		ShortestWay w = g.getShortestPathDjikstra(0, 5);
+		long end = System.nanoTime();
 		System.out.println(w.totalCost);
 		for (Iterator<Integer> it = w.nodes.iterator(); it.hasNext();) {
 			int type = (int) it.next();
 			System.out.println(type);
 		}
+		System.out.println((end - start)/1e9);
 		
 	}
+	
+	public static EdgeGraph getGraph(int nodes, int edges, double maxWeight) {
+		EdgeGraph g = new EdgeGraph(nodes);
+		Random r = new Random();
+		for (int i = 0; i < nodes; i++) {
+			g.addNode();
+		}
+		for (int i = 0; i < edges; i++) {
+			while(!g.createEdge(r.nextInt(nodes),r.nextInt(nodes), r.nextDouble() * maxWeight, false));
+		}
+		return g;
+	}
+	*/
 }
